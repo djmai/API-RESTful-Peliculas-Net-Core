2816d236dcdfaf8d3ef3b652343bd7d191c5e45e
Miguel Martínez <mai.djmai@gmail.com>
2023-10-22 22:53:02 -0600

    Agregando buscar genero por id

--- PeliculasAPI/Controllers/GenerosController.cs
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -8,13 +8,13 @@ namespace PeliculasAPI.Controllers
 {
 	[ApiController]
 	[Route("api/generos")]
-	public class GenerosController: ControllerBase
+	public class GenerosController : ControllerBase
 	{
 		private readonly ApplicationDbContext context;
 		private readonly IMapper mapper;
 
 		public GenerosController(ApplicationDbContext context, IMapper mapper)
-        {
+		{
 			this.context = context;
 			this.mapper = mapper;
 		}
@@ -26,5 +26,17 @@ namespace PeliculasAPI.Controllers
 			var dtos = mapper.Map<List<GeneroDTO>>(entidades);
 			return dtos;
 		}
-    }
+
+		[HttpGet("{id:int}")]
+		public async Task<ActionResult<GeneroDTO>> Get(int id)
+		{
+			var entidad = await context.Generos.FirstOrDefaultAsync(x => x.Id == id);
+			if (entidad == null)
+				return NotFound();
+
+			var dto = mapper.Map<GeneroDTO>(entidad);
+
+			return dto;
+		}
+	}
 }
