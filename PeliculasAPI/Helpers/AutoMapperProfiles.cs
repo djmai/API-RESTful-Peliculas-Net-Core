e9b39cbd3c63d212b0f75a82a7eb598183301cf0
Miguel Martínez <mai.djmai@gmail.com>
2023-10-23 20:33:14 -0600

    Agregando ignoracion de mapeo de Foto

--- PeliculasAPI/Helpers/AutoMapperProfiles.cs
+++ PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -4,15 +4,15 @@ using PeliculasAPI.Entidades;
 
 namespace PeliculasAPI.Helpers
 {
-	public class AutoMapperProfiles: Profile
+	public class AutoMapperProfiles : Profile
 	{
-        public AutoMapperProfiles()
-        {
-            CreateMap<Genero, GeneroDTO>().ReverseMap();
-            CreateMap<GeneroCreacionDTO, Genero>();
+		public AutoMapperProfiles()
+		{
+			CreateMap<Genero, GeneroDTO>().ReverseMap();
+			CreateMap<GeneroCreacionDTO, Genero>();
 
-            CreateMap<Actor, ActorDTO>().ReverseMap();
-            CreateMap<ActorCreacionDTO, Actor>();
+			CreateMap<Actor, ActorDTO>().ReverseMap();
+			CreateMap<ActorCreacionDTO, Actor>().ForMember(x => x.Foto, options => options.Ignore());
 		}
-    }
+	}
 }
