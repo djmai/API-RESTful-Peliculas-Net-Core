33e95930efddd6efd8c22575c53c119fa32e6d80
Miguel Martínez <mai.djmai@gmail.com>
2023-10-23 20:37:40 -0600

    Actualizacion de metodos POST y PUT para guardar Foto

--- PeliculasAPI/Controllers/ActoresController.cs
+++ PeliculasAPI/Controllers/ActoresController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PeliculasAPI.DTOs;
 using PeliculasAPI.Entidades;
+using PeliculasAPI.Servicios;
 
 namespace PeliculasAPI.Controllers
 {
@@ -12,11 +13,14 @@ namespace PeliculasAPI.Controllers
 	{
 		private readonly ApplicationDbContext context;
 		private readonly IMapper mapper;
+		private readonly IAlmacenadorArchivos almacenadorArchivos;
+		private readonly string contenedor = "actores";
 
-		public ActoresController(ApplicationDbContext context, IMapper mapper)
+		public ActoresController(ApplicationDbContext context, IMapper mapper, IAlmacenadorArchivos almacenadorArchivos)
 		{
 			this.context = context;
 			this.mapper = mapper;
+			this.almacenadorArchivos = almacenadorArchivos;
 		}
 
 		[HttpGet]
@@ -43,32 +47,52 @@ namespace PeliculasAPI.Controllers
 		public async Task<ActionResult> Post([FromForm] ActorCreacionDTO actorCreacionDTO)
 		{
 			var entidad = mapper.Map<Actor>(actorCreacionDTO);
+
+			if (actorCreacionDTO.Foto != null)
+			{
+				using (var memoryStream = new MemoryStream())
+				{
+					await actorCreacionDTO.Foto.CopyToAsync(memoryStream); // Copiando hacia el memoryStream
+					var contenido = memoryStream.ToArray(); // Guardando bytes en variable
+					var extension = Path.GetExtension(actorCreacionDTO.Foto.FileName); // Extrayendo extension del archivo
+					entidad.Foto = await almacenadorArchivos.GuardarArchivo(contenido, extension, contenedor, actorCreacionDTO.Foto.ContentType);// Guardamos string de la url
+				}
+			}
+
 			context.Add(entidad);
-			//await context.SaveChangesAsync();
+			await context.SaveChangesAsync(); // Guardando registro en la DB
 
 			var dto = mapper.Map<ActorDTO>(entidad);
 			return new CreatedAtRouteResult("obtenerActor", new { id = entidad.Id }, dto);
 		}
 
 		[HttpPut]
-		public async Task<ActionResult> Put(int id, [FromBody] GeneroCreacionDTO generoCreacionDTO)
+		public async Task<ActionResult> Put(int id, [FromForm] ActorCreacionDTO actorCreacionDTO)
 		{
-			var entidad = mapper.Map<Genero>(generoCreacionDTO);
-			entidad.Id = id;
-			context.Entry(entidad).State = EntityState.Modified;
+			// Actualiza todos los datos cuando el usuario envia todo lo de ActorCreacionDTO
+			// var entidad = mapper.Map<Actor>(actorCreacionDTO);
+			// entidad.Id = id;
+			// context.Entry(entidad).State = EntityState.Modified;
+
+			// Actualizacion de los campos que son diferentes
+			var actorDB = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+
+			if (actorDB == null) { return NotFound(); }
+
+			actorDB = mapper.Map(actorCreacionDTO, actorDB); // Mapedo de campos de actorCreacionDTO a actorDB - solo los que traigan dato
+
+			if (actorCreacionDTO.Foto != null)
+			{
+				using (var memoryStream = new MemoryStream())
+				{
+					await actorCreacionDTO.Foto.CopyToAsync(memoryStream); // Copiando hacia el memoryStream
+					var contenido = memoryStream.ToArray(); // Guardando bytes en variable
+					var extension = Path.GetExtension(actorCreacionDTO.Foto.FileName); // Extrayendo extension del archivo
+					actorDB.Foto = await almacenadorArchivos.EditarArchivo(contenido, extension, contenedor, actorDB.Foto, actorCreacionDTO.Foto.ContentType);// Guardamos string de la url
+				}
+			}
+
 			await context.SaveChangesAsync();
-
-			return NoContent();
-		}
-
-		[HttpPut]
-		public async Task<ActionResult> Put(int id, [FromBody] ActorCreacionDTO actorCreacionDTO)
-		{
-			var entidad = mapper.Map<Actor>(actorCreacionDTO);
-			entidad.Id = id;
-			context.Entry(entidad).State = EntityState.Modified;
-			await context.SaveChangesAsync();
-
 			return NoContent();
 		}
 
