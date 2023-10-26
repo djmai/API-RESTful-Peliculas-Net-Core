e5c31ff80a12866013da14723233d4242fa5ab06
Miguel Martínez <mai.djmai@gmail.com>
2023-10-25 22:15:23 -0600

    Revert "Actualizando metodos PUT y PATCH"
    This reverts commit 747c8c94eab1cf4a8ef8d93603ba2825cc6a7fa7.

--- PeliculasAPI/DTOs/PeliculaCreacionDTO.cs
+++ PeliculasAPI/DTOs/PeliculaCreacionDTO.cs
@@ -3,8 +3,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PeliculasAPI.DTOs
 {
-	public class PeliculaCreacionDTO : PeliculaPatchDTO
+	public class PeliculaCreacionDTO
 	{
+		[Required]
+		[StringLength(300)]
+		public string Titulo { get; set; }
+
+		public bool EnCines { get; set; }
+
+		public DateTime FechaEstreno { get; set; }
+
 		[PesoArchivoValidacion(PesoMaximoEnMegaBytes: 4)]
 		[TipoArchivoValidacion(GrupoTipoArchivo.Imagen)]
 		public IFormFile Poster { get; set; }
