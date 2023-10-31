a53e838a33cc18bbed68d8a7de0c1016c5bed44e
Miguel Martínez <mai.djmai@gmail.com>
2023-10-31 15:29:42 -0600

    Agregando listado de ActorPeliculaDetalleDTO

---
+++ PeliculasAPI/DTOs/PeliculaDetallesDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PeliculasAPI.DTOs
+{
+	public class PeliculaDetallesDTO : PeliculaDTO
+	{
+        public List<GeneroDTO> Generos { get; set; }
+
+        public List<ActorPeliculaDetalleDTO> Actores { get; set; }
+    }
+}
