39240b4077bbd15b5c6090a8ecfd8f5839a96db2
Miguel Martínez <mai.djmai@gmail.com>
2023-10-31 14:41:44 -0600

    Agregando DTO de peliculas de estreno y en cines

---
+++ PeliculasAPI/DTOs/PeliculasIndexDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PeliculasAPI.DTOs
+{
+	public class PeliculasIndexDTO
+	{
+		public List<PeliculaDTO> FuturosEstrenos { get; set; }
+
+		public List<PeliculaDTO> EnCines { get; set; }
+	}
+}
