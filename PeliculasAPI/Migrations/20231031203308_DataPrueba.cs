936dc32be91bda218c693334d0f25297e13ac99e
Miguel Martínez <mai.djmai@gmail.com>
2023-10-31 14:34:12 -0600

    Agregando migración de data de prueba

---
+++ PeliculasAPI/Migrations/20231031203308_DataPrueba.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PeliculasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataPrueba : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actores",
+                columns: new[] { "Id", "FechaNacimiento", "Foto", "Nombre" },
+                values: new object[,]
+                {
+                    { 3, new DateTime(1962, 1, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Jim Carrey" },
+                    { 4, new DateTime(1965, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Robert Downey Jr." },
+                    { 5, new DateTime(1981, 6, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Chris Evans" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Generos",
+                columns: new[] { "Id", "Nombre" },
+                values: new object[,]
+                {
+                    { 5, "Aventura" },
+                    { 6, "Animación" },
+                    { 7, "Suspenso" },
+                    { 8, "Romance" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Peliculas",
+                columns: new[] { "Id", "EnCines", "FechaEstreno", "Poster", "Titulo" },
+                values: new object[,]
+                {
+                    { 4, true, new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Avengers: Endgame" },
+                    { 5, false, new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Avengers: Infinity Wars" },
+                    { 6, false, new DateTime(2020, 2, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Sonic the Hedgehog" },
+                    { 7, false, new DateTime(2020, 2, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Emma" },
+                    { 8, false, new DateTime(2020, 8, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Wonder Woman 1984" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PeliculasActores",
+                columns: new[] { "ActorId", "PeliculaId", "Orden", "Personaje" },
+                values: new object[,]
+                {
+                    { 3, 6, 1, "Dr. Ivo Robotnik" },
+                    { 4, 4, 1, "Tony Stark" },
+                    { 4, 5, 1, "Tony Stark" },
+                    { 5, 4, 2, "Steve Rogers" },
+                    { 5, 5, 2, "Steve Rogers" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PeliculasGeneros",
+                columns: new[] { "GeneroId", "PeliculaId" },
+                values: new object[,]
+                {
+                    { 5, 4 },
+                    { 5, 5 },
+                    { 5, 6 },
+                    { 5, 8 },
+                    { 7, 4 },
+                    { 7, 5 },
+                    { 7, 7 },
+                    { 7, 8 },
+                    { 8, 7 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 3, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 4, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 5, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 5, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 5, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 5, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 7, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 7, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 7, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 7, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 8, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "Actores",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Actores",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Actores",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
