897b07dd2076ba4894e1268a33fee45a82c1ab98
Miguel Martínez <mai.djmai@gmail.com>
2023-11-01 17:45:14 -0600

    Agergando BasePruebas

---
+++ PeliculasAPI.Tests/BasePruebas.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NetTopologySuite;
+using PeliculasAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace PeliculasAPI.Tests
+{
+	public class BasePruebas
+	{
+		protected ApplicationDbContext ConstruirContext(string nombreDB)
+		{
+			var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(nombreDB).Options;
+
+			var dbContext = new ApplicationDbContext(opciones);
+			return dbContext;
+		}
+
+		protected IMapper ConfigurarAutoMapper()
+		{
+			var config = new MapperConfiguration(options =>
+			{
+				var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+				options.AddProfile(new AutoMapperProfiles(geometryFactory));
+			});
+
+			return config.CreateMapper();
+		}
+	}
+}
