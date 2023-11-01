085bb7fc20abc37c61f8d0d83cfdc77474e6ecb8
Miguel Martínez <mai.djmai@gmail.com>
2023-10-31 19:20:42 -0600

    Agregando mapeo de SalaDeCine

--- PeliculasAPI/Helpers/AutoMapperProfiles.cs
+++ PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -8,30 +8,30 @@ namespace PeliculasAPI.Helpers
 	{
 		public AutoMapperProfiles()
 		{
+			// Mapeo de Genero
 			CreateMap<Genero, GeneroDTO>().ReverseMap();
-
 			CreateMap<GeneroCreacionDTO, Genero>();
 
-			CreateMap<Actor, ActorDTO>().ReverseMap();
+			// Mapeo de SalaDeCine
+			CreateMap<SalaDeCine, SalaDeCineDTO>().ReverseMap();
+			CreateMap<SalaDeCineCreacionDTO, SalaDeCine>();
 
+			// Mapeo de Actor
+			CreateMap<Actor, ActorDTO>().ReverseMap();
 			CreateMap<ActorCreacionDTO, Actor>()
 				.ForMember(x => x.Foto, options => options.Ignore());
-
 			CreateMap<ActorPatchDTO, Actor>().ReverseMap();
 
+			//Mapeo de Pelicula
 			CreateMap<Pelicula, PeliculaDTO>().ReverseMap();
-
 			CreateMap<PeliculaCreacionDTO, Pelicula>()
 				.ForMember(x => x.Poster, options => options.Ignore())
 				.ForMember(x => x.PeliculasGeneros, options => options.MapFrom(MapPeliculasGeneros))
 				.ForMember(x => x.PeliculasActores, options => options.MapFrom(MapPeliculasActores));
-
 			CreateMap<Pelicula, PeliculaDetallesDTO>()
 				.ForMember(x => x.Generos, options => options.MapFrom(MapPeliculasGeneros))
 				.ForMember(x => x.Actores, options => options.MapFrom(MapPeliculasActores));
-
 			CreateMap<PeliculaPatchDTO, Pelicula>().ReverseMap();
-
 
 		}
 
