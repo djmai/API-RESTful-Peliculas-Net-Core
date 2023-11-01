3652df714d1db8a9d4b5f16bd0171ba3c114e596
Miguel Martínez <mai.djmai@gmail.com>
2023-10-31 20:07:25 -0600

    Agregando automapeo de GeometryFactory

--- PeliculasAPI/Startup.cs
+++ PeliculasAPI/Startup.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Utilities;
+using PeliculasAPI.Helpers;
 using PeliculasAPI.Servicios;
 
 namespace PeliculasAPI
@@ -23,6 +28,17 @@ namespace PeliculasAPI
 			// Agregando servicio para almacenar archivos en local
 			services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
 			services.AddHttpContextAccessor();
+
+			// Automapeo de GeometryFactory
+			services.AddSingleton<GeometryFactory>(NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326));
+
+			services.AddSingleton(provider =>
+				new MapperConfiguration(config =>
+				{
+					var geometryFactory = provider.GetRequiredService<GeometryFactory>();
+					config.AddProfile(new AutoMapperProfiles(geometryFactory));
+				}).CreateMapper()
+			);
 
 			// Configuración de conexión con la base de datos
 			services.AddDbContext<ApplicationDbContext>(
