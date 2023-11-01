b53860fa40e4bd8afb866cbad41eff79b4bed2c2
Miguel Martínez <mai.djmai@gmail.com>
2023-10-31 19:41:32 -0600

    Agregando campo de Ubicacion

--- PeliculasAPI/Entidades/SalaDeCine.cs
+++ PeliculasAPI/Entidades/SalaDeCine.cs
@@ -1,17 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using NetTopologySuite.Geometries;
+using System.ComponentModel.DataAnnotations;
 
 namespace PeliculasAPI.Entidades
 {
-	public class SalaDeCine: IId
+	public class SalaDeCine : IId
 	{
 
-        public int Id { get; set; }
+		public int Id { get; set; }
 
-        [Required]
-        [StringLength(120)]
-        public string Nombre { get; set; }
+		[Required]
+		[StringLength(120)]
+		public string Nombre { get; set; }
 
-        public List<PeliculasSalasDeCine> PeliculasSalasDeCine { get; set; }
+		public Point Ubicacion { get; set; }
 
-    }
+		public List<PeliculasSalasDeCine> PeliculasSalasDeCine { get; set; }
+
+	}
 }
