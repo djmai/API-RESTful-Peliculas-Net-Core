635f60bcfde4a54e6764ddca187f91ed7e9ced03
Miguel Martínez <mai.djmai@gmail.com>
2023-11-01 15:25:27 -0600

    Agregando migraciones de datos de Admin

---
+++ PeliculasAPI/Migrations/20231101211441_AdminData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeliculasAPI.Migrations
+{
+	/// <inheritdoc />
+	public partial class AdminData : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql(@"IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ConcurrencyStamp', N'Name', N'NormalizedName') AND [object_id] = OBJECT_ID(N'[AspNetRoles]'))
+    SET IDENTITY_INSERT [AspNetRoles] ON;
+INSERT INTO [AspNetRoles] ([Id], [ConcurrencyStamp], [Name], [NormalizedName])
+VALUES (N'b29e33de-5979-42b7-8cc8-8fb8ade5c851', N'd08523ea-ef52-4b99-9ba6-e2c1f534808f', N'Admin', N'Admin');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ConcurrencyStamp', N'Name', N'NormalizedName') AND [object_id] = OBJECT_ID(N'[AspNetRoles]'))
+    SET IDENTITY_INSERT [AspNetRoles] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'AccessFailedCount', N'ConcurrencyStamp', N'Email', N'EmailConfirmed', N'LockoutEnabled', N'LockoutEnd', N'NormalizedEmail', N'NormalizedUserName', N'PasswordHash', N'PhoneNumber', N'PhoneNumberConfirmed', N'SecurityStamp', N'TwoFactorEnabled', N'UserName') AND [object_id] = OBJECT_ID(N'[AspNetUsers]'))
+    SET IDENTITY_INSERT [AspNetUsers] ON;
+INSERT INTO [AspNetUsers] ([Id], [AccessFailedCount], [ConcurrencyStamp], [Email], [EmailConfirmed], [LockoutEnabled], [LockoutEnd], [NormalizedEmail], [NormalizedUserName], [PasswordHash], [PhoneNumber], [PhoneNumberConfirmed], [SecurityStamp], [TwoFactorEnabled], [UserName])
+VALUES (N'e16539eb-47d7-4d00-beac-1ad1e3cbb84f', 0, N'3169a3dc-32b0-43e0-a824-e9059fecea59', N'admin@example.com', CAST(0 AS bit), CAST(0 AS bit), NULL, N'admin@example.com', N'admin@example.com', N'AQAAAAEAACcQAAAAENjP6tFQVKdi+Mrn0OyGe43JZE8qP0dYKwjq5L/CEieU3O65buT4Sy1HjdFtSRvWZw==', NULL, CAST(0 AS bit), N'db97aaff-fcc5-44e0-b6f3-a1f70da893bb', CAST(0 AS bit), N'admin@example.com');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'AccessFailedCount', N'ConcurrencyStamp', N'Email', N'EmailConfirmed', N'LockoutEnabled', N'LockoutEnd', N'NormalizedEmail', N'NormalizedUserName', N'PasswordHash', N'PhoneNumber', N'PhoneNumberConfirmed', N'SecurityStamp', N'TwoFactorEnabled', N'UserName') AND [object_id] = OBJECT_ID(N'[AspNetUsers]'))
+    SET IDENTITY_INSERT [AspNetUsers] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ClaimType', N'ClaimValue', N'UserId') AND [object_id] = OBJECT_ID(N'[AspNetUserClaims]'))
+    SET IDENTITY_INSERT [AspNetUserClaims] ON;
+INSERT INTO [AspNetUserClaims] ([Id], [ClaimType], [ClaimValue], [UserId])
+VALUES (1, N'http://schemas.microsoft.com/ws/2008/06/identity/claims/role', N'Admin', N'e16539eb-47d7-4d00-beac-1ad1e3cbb84f');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ClaimType', N'ClaimValue', N'UserId') AND [object_id] = OBJECT_ID(N'[AspNetUserClaims]'))
+    SET IDENTITY_INSERT [AspNetUserClaims] OFF;
+GO
+");
+
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DeleteData(
+				table: "AspNetRoles",
+				keyColumn: "Id",
+				keyValue: "b29e33de-5979-42b7-8cc8-8fb8ade5c851");
+
+			migrationBuilder.DeleteData(
+				table: "AspNetUserClaims",
+				keyColumn: "Id",
+				keyValue: 1);
+
+			migrationBuilder.DeleteData(
+				table: "AspNetUsers",
+				keyColumn: "Id",
+				keyValue: "e16539eb-47d7-4d00-beac-1ad1e3cbb84f");
+		}
+	}
+}
