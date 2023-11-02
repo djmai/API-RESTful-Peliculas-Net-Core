e975ece16c4d1efd20f96caa6fcf71490e19aaa9
Miguel Martínez <mai.djmai@gmail.com>
2023-11-01 18:28:01 -0600

    Agregando ConstruirControllerContext

--- PeliculasAPI.Tests/BasePruebas.cs
+++ PeliculasAPI.Tests/BasePruebas.cs
@@ -16,6 +16,9 @@ namespace PeliculasAPI.Tests
 {
 	public class BasePruebas
 	{
+		protected string usuarioPorDefectoId = "9722b56a-77ea-4e41-941d-e319b6eb3712";
+		protected string usuarioPorDefectoEmail = "ejemplo@hotmail.com";
+
 		protected ApplicationDbContext ConstruirContext(string nombreDB)
 		{
 			var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -34,6 +37,21 @@ namespace PeliculasAPI.Tests
 			});
 
 			return config.CreateMapper();
+		}
+
+		protected ControllerContext ConstruirControllerContext()
+		{
+			var usuario = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+			{
+				new Claim(ClaimTypes.Name, usuarioPorDefectoEmail),
+				new Claim(ClaimTypes.Email, usuarioPorDefectoEmail),
+				new Claim(ClaimTypes.NameIdentifier, usuarioPorDefectoId)
+			}));
+
+			return new ControllerContext()
+			{
+				HttpContext = new DefaultHttpContext() { User = usuario }
+			};
 		}
 	}
 }
